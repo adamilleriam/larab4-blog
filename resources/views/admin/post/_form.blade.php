fbe5a12d3f37718a97c26bdaf6b823a30415f523
nur165024 <nur165024@gmail.com>
2019-10-05 16:03:49 +0600

    Post edit form done

--- resources/views/admin/post/_form.blade.php
+++ resources/views/admin/post/_form.blade.php
@@ -3,19 +3,20 @@
     <select name="category_id" class="form-control" id="category">
         <option value="">Select Category</option>
         @foreach($categories as $category)
-            <option @if(old('category_id') == $category->id) selected @endif value="{{ $category->id }}">{{ $category->name }}</option>
+            <option @if(old('category_id',isset($post)?$post->category_id:null)) selected @endif value="{{ $category->id }}">{{ $category->name }}</option>
         @endforeach
     </select>
     @error('category_id')
     <div class="text-danger">{{ $message }}</div>
     @enderror
 </div>
+
 <div class="form-group">
     <label for="author">Author</label>
     <select name="author_id" class="form-control" id="author">
         <option value="">Select Author</option>
         @foreach($authors as $author)
-            <option @if(old('author_id') == $author->id) selected @endif value="{{ $author->id }}">{{ $author->name }}</option>
+            <option @if(old('author_id',isset($post)?$post->author_id:null)) selected @endif value="{{ $author->id }}">{{ $author->name }}</option>
         @endforeach
     </select>
     @error('author_id')
@@ -40,14 +41,14 @@
     <label for="#">Status</label>
     <div class="form-check">
         <label class="form-check-label">
-            <input @if(old('status') == 'published') checked @endif value="published" type="radio" class="form-check-input" name="status" id="published">
+            <input @if(old('status',isset($post)?$post->status:null) == 'published') checked @endif value="published" type="radio" class="form-check-input" name="status" id="published">
             Published
         </label>
     </div>
     <div class="form-check">
 
         <label class="form-check-label">
-            <input @if(old('status') == 'unpublished') checked @endif value="unpublished" type="radio" class="form-check-input" name="status" id="unpublished">
+            <input @if(old('status',isset($post)?$post->status:null) == 'unpublished') checked @endif value="unpublished" type="radio" class="form-check-input" name="status" id="unpublished">
             Unpublished
         </label>
     </div>
@@ -61,8 +62,8 @@
     <div class="input-group col-xs-12">
         <input name="image" type="text" class="form-control file-upload-info" disabled placeholder="Upload Image">
         <span class="input-group-append">
-                          <button class="file-upload-browse btn btn-primary" type="button">Upload</button>
-                        </span>
+          <button class="file-upload-browse btn btn-primary" type="button">Upload</button>
+        </span>
     </div>
     @error('image')
     <div class="text-danger">{{ $message }}</div>
