88e9b9e6fb159bd17a55ae29b8ae4955d27f3246
Trainer PNT <trainer@piit.local>
2019-10-06 20:15:13 +0600

    Frontend templating done

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/','HomeController@index');
 
 Route::get('login','LoginController@index')->name('user.login');
 Route::post('login','LoginController@login')->name('login');
