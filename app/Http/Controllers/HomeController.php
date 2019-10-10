336993f9cd293996735654848ae37483db41ea0f
Trainer PNT <trainer@piit.local>
2019-10-10 20:51:03 +0600

    Home page done

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
 use App\Post;
 use Illuminate\Http\Request;
 
@@ -10,6 +11,8 @@ class HomeController extends Controller
     public function index(){
         $data['featured_posts'] = Post::with(['category','author'])->published()->where('is_featured',1)->get();
         $data['latest_posts'] = Post::with(['category','author'])->published()->orderBy('id','desc')->paginate(8);
+        $data['popular_posts'] = Post::published()->orderBy('total_hit','desc')->limit(3)->get();
+        $data['categories'] = Category::all();
 //        dd($data['latest_posts']);
         return view('front.index',$data);
     }
